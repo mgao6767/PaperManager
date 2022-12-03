f96689d40d27bcf9023907e004bd3277fadeed8c
Adrian Gao <mingze.gao@sydney.edu.au>
2022-12-03 14:58:00 +1100

    🔨 [dev] Tags column in files viewer

--- src/PaperManager/components/filesystem_viewer/fsviewer.py
+++ src/PaperManager/components/filesystem_viewer/fsviewer.py
@@ -1,6 +1,6 @@
 from PyQt6.QtWidgets import QDockWidget, QTreeView, QHeaderView
-from PyQt6.QtGui import QFileSystemModel
-from PyQt6.QtCore import QDir, Qt
+from PyQt6.QtGui import QFileSystemModel, QColor
+from PyQt6.QtCore import QDir, Qt, QModelIndex
 
 from ..signals import PMCommunicate
 
@@ -18,6 +18,44 @@ class FSTreeView(QTreeView):
         super().currentChanged(current, previous)
 
 
+class FSModel(QFileSystemModel):
+    def __init__(self, parent) -> None:
+        super().__init__(parent)
+        # Show only PDF files
+        self.setNameFilters(["*.pdf", "*.PDF"])
+        self.setNameFilterDisables(False)
+
+    def columnCount(self, parent=QModelIndex()):
+        # Add one more column for tags
+        return super().columnCount(parent) + 1
+
+    def headerData(self, section, orientation, role=Qt.ItemDataRole.DisplayRole):
+        if (
+            orientation == Qt.Orientation.Horizontal
+            and role == Qt.ItemDataRole.DisplayRole
+            and section == self.columnCount() - 1
+        ):
+            return "Tags"
+        return super().headerData(section, orientation, role)
+
+    def data(self, index, role=Qt.ItemDataRole.DisplayRole):
+        isTagCol = False
+        if index.isValid():
+            isTagCol = index.column() == self.columnCount(index.parent()) - 1
+            info = self.fileInfo(index)
+            path = info.absoluteFilePath()
+            if path:
+                if isTagCol:
+                    if role == Qt.ItemDataRole.DecorationRole:
+                        pass
+                    elif role == Qt.ItemDataRole.DisplayRole:
+                        return f"JFE; Open Access;"
+                    elif role == Qt.ItemDataRole.ForegroundRole:
+                        return QColor("blue")
+        if not isTagCol:
+            return super().data(index, role)
+
+
 class FSViewer(QDockWidget):
     def __init__(self, parent, comm, *args, **kwargs) -> None:
         super().__init__("File System", parent, *args, **kwargs)
@@ -29,13 +67,18 @@ class FSViewer(QDockWidget):
             | QDockWidget.DockWidgetFeature.DockWidgetClosable
         )
 
-        self.fsmodel = QFileSystemModel(self)
+        self.fsmodel = FSModel(self)
         self.fsmodel.setRootPath(QDir.homePath())
         self.treeView = FSTreeView(self, comm)
         self.treeView.setModel(self.fsmodel)
+        # self.treeView.header().moveSection(self.fsmodel.columnCount() - 1, 1)
+        self.treeView.header().hideSection(1)  # size
+        self.treeView.header().hideSection(2)  # type
+        self.treeView.header().hideSection(3)  # date modified
         self.treeView.header().setSectionResizeMode(
             0, QHeaderView.ResizeMode.ResizeToContents
         )
+        self.treeView.setAlternatingRowColors(True)
         self.setWidget(self.treeView)
 
     def set_dir(self, path: str) -> None:
