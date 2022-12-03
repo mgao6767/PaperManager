a1c3f4802cf86874057ac06e5b67b25220b9f7a9
Adrian Gao <mingze.gao@sydney.edu.au>
2022-12-03 19:38:56 +1100

    🔨 [dev] Lower required Python version

--- src/PaperManager/__init__.py
+++ src/PaperManager/__init__.py
@@ -4,6 +4,6 @@ __version__ = "0.0.1.dev1"
 __author__ = "Mingze Gao"
 __homepage__ = "https://github.com/mgao6767/PaperManager"
 
-if sys.version_info.major < 3 and sys.version_info.minor < 10:
-    print("Python3.10 or higher is required.")
+if sys.version_info.major < 3 and sys.version_info.minor < 9:
+    print("Python3.9 or higher is required.")
     sys.exit(1)
